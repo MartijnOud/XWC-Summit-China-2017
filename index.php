f4773d6137ed10200506d044eb6123154e21e1ce
Martijn <oudmartijn@gmail.com>
2017-11-29 22:54:01 +0100

    Add countdown timer

--- index.php
+++ index.php
@@ -28,7 +28,6 @@ $Parsedown = new Parsedown();
 
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css">
 <link rel="stylesheet" type="text/css" href="/css/main.css">
 </head>
@@ -44,14 +43,45 @@ $Parsedown = new Parsedown();
 </section>
 
 <section class="section">
-    <div class="container">
-        <h2 class="title"><?php echo $Parsedown->text($Translate->__('about-the-xwc-summit'));?></h2>
-        <?php echo $Parsedown->text($Translate->__('about-the-xwc-summit-introduction--markdown'));?>
+    <div class="container content">
+        <div class="tile is-ancestor">
+            <div class="tile is-child is-7">
+                <h1 class="title"><?php echo $Translate->__('about-the-xwc-summit');?></h1>
+                <?php echo $Parsedown->text($Translate->__('about-the-xwc-summit-introduction--markdown'));?>
+            </div>
+            <div class="tile is-vertical is-parent">
+                <div class="tile box is-child">
+                    <h2 class="subtitle"><?php echo $Translate->__('where');?></h2>
+                    <?php echo $Parsedown->text($Translate->__('where-details--markdown'));?>
+                </div>
+                <div class="tile box is-child">
+                    <h2 class="subtitle"><?php echo $Translate->__('when');?></h2>
+                    <div class="countdown">
+                        <div class="countdown-days">
+                            <span class="countdown-days-amount">00</span>
+                            <span><?php echo $Translate->__('days');?></span>
+                        </div>
+                        <div class="countdown-hours">
+                            <span class="countdown-hours-amount">00</span>
+                            <span><?php echo $Translate->__('hours');?></span>
+                        </div>
+                        <div class="countdown-minutes">
+                            <span class="countdown-minutes-amount">00</span>
+                            <span><?php echo $Translate->__('minutes');?></span>
+                        </div>
+                        <div class="countdown-seconds">
+                            <span class="countdown-seconds-amount">00</span>
+                            <span><?php echo $Translate->__('seconds');?></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 
 <section class="section">
-    <div class="container">
+    <div class="container content">
         <h2 class="title"><?php echo $Translate->__('speakers');?></h2>
         <div class="columns">
             <div class="column">
@@ -79,7 +109,7 @@ $Parsedown = new Parsedown();
 </section>
 
 <section class="section">
-    <div class="container">
+    <div class="container content">
         <h2 class="title"><?php echo $Translate->__('details');?></h2>
         <?php echo $Parsedown->text($Translate->__('details-venue-meeting--markdown'));?>
         <?php echo $Parsedown->text($Translate->__('details-venue-banquet--markdown'));?>
@@ -88,11 +118,33 @@ $Parsedown = new Parsedown();
     </div>
 </section>
 
+<section class="section">
+    <!-- @todo: call to action? -->
+</section>
+
 <script>
 // lazy load full screen image
 (function() {
     document.getElementsByClassName('hero')[0].classList.add('hero-background');
 })();
+
+var countDownDate = new Date("Dec 10, 2017 13:30:00").getTime();
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementsByClassName('countdown-days-amount')[0].innerHTML = days;
+  document.getElementsByClassName('countdown-hours-amount')[0].innerHTML = hours;
+  document.getElementsByClassName('countdown-minutes-amount')[0].innerHTML = minutes;
+  document.getElementsByClassName('countdown-seconds-amount')[0].innerHTML = seconds;
+
+}, 1000);
 </script>
 </body>
 </html>
