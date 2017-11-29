686c5c8235d3999069685593d5601c10699d24b9
Martijn <oudmartijn@gmail.com>
2017-11-29 20:56:23 +0100

    Add Markdown support

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
  */
 
 include 'classes/Locale.php';
+include 'classes/Parsedown.php';
 
-$locale = $_GET['language'];
+$locale = strtolower(trim($_GET['language']));
 $arrLocales = ['en'];
 
 // default to english
@@ -14,6 +15,8 @@ if (!in_array($locale, $arrLocales)) {
     $locale = 'en';
 }
 $Translate = new MartijnOud\Locale($locale);
+
+$Parsedown = new Parsedown();
 ?>
 <!DOCTYPE html>
 <html lang="<?php echo htmlspecialchars($locale); ?>">
@@ -30,42 +33,43 @@ $Translate = new MartijnOud\Locale($locale);
 <section class="hero is-fullheight">
   <div class="hero-body">
     <div class="container has-text-centered">
-      <h1 class="title"><?php echo $Translate->__('xwc-summit-china-2017');?></h1>
-      <h2 class="subtitle"><?php echo $Translate->__('2017-10-12-shanghai-china');?></h2>
+      <h1 class="title"><?php echo $Parsedown->text($Translate->__('xwc-summit-china-2017'));?></h1>
+      <h2 class="subtitle"><?php echo $Parsedown->text($Translate->__('2017-10-12-shanghai-china'));?></h2>
     </div>
   </div>
 </section>
 
 <section class="section">
     <div class="container">
-        <h2 class="title"><?php echo $Translate->__('about-the-xwc-summit');?></h2>
-        <p><?php echo $Translate->__('about-the-xwc-summit-introduction');?></p>
+        <h2 class="title"><?php echo $Parsedown->text($Translate->__('about-the-xwc-summit'));?></h2>
+        <p><?php echo $Parsedown->text($Translate->__('about-the-xwc-summit-introduction'));?></p>
+        <p><?php echo $Parsedown->text($Translate->__('about-the-xwc-summit-continued'));?></p>
     </div>
 </section>
 
 <section class="section">
     <div class="container">
-        <h2 class="title"><?php echo $Translate->__('Speakers');?></h2>
+        <h2 class="title"><?php echo $Parsedown->text($Translate->__('Speakers'));?></h2>
         <div class="columns">
             <div class="column">
-                <h3 class="subtitle"><?php echo $Translate->__('oizopower');?></h3>
-                <img src="http://placehold.it/150x150" alt="<?php echo $Translate->__('oizopower');?>">
-                <p><?php echo $Translate->__('oizopower-introduction');?></p>
+                <h3 class="subtitle"><?php echo $Parsedown->text($Translate->__('oizopower'));?></h3>
+                <img src="http://placehold.it/500x500" alt="<?php echo $Parsedown->text($Translate->__('oizopower'));?>">
+                <p><?php echo $Parsedown->text($Translate->__('oizopower-introduction'));?></p>
             </div>
             <div class="column">
-                <h3 class="subtitle"><?php echo $Translate->__('lizhi');?></h3>
-                <img src="http://placehold.it/150x150" alt="<?php echo $Translate->__('lizhi');?>">
-                <p><?php echo $Translate->__('lizhi-introduction');?></p>
+                <h3 class="subtitle"><?php echo $Parsedown->text($Translate->__('lizhi'));?></h3>
+                <img src="http://placehold.it/500x500" alt="<?php echo $Parsedown->text($Translate->__('lizhi'));?>">
+                <p><?php echo $Parsedown->text($Translate->__('lizhi-introduction'));?></p>
             </div>
             <div class="column">
-                <h3 class="subtitle"><?php echo $Translate->__('waligu');?></h3>
-                <img src="http://placehold.it/150x150" alt="<?php echo $Translate->__('waligu');?>">
-                <p><?php echo $Translate->__('waligu-introduction');?></p>
+                <h3 class="subtitle"><?php echo $Parsedown->text($Translate->__('waligu'));?></h3>
+                <img src="http://placehold.it/500x500" alt="<?php echo $Parsedown->text($Translate->__('waligu'));?>">
+                <p><?php echo $Parsedown->text($Translate->__('waligu-introduction'));?></p>
             </div>
             <div class="column">
-                <h3 class="subtitle"><?php echo $Translate->__('will');?></h3>
-                <img src="http://placehold.it/150x150" alt="<?php echo $Translate->__('will');?>">
-                <p><?php echo $Translate->__('will-introduction');?></p>
+                <h3 class="subtitle"><?php echo $Parsedown->text($Translate->__('will'));?></h3>
+                <img src="http://placehold.it/500x500" alt="<?php echo $Parsedown->text($Translate->__('will'));?>">
+                <p><?php echo $Parsedown->text($Translate->__('will-introduction'));?></p>
             </div>
         </div>
     </div>
@@ -73,7 +77,11 @@ $Translate = new MartijnOud\Locale($locale);
 
 <section class="section">
     <div class="container">
-        <h2 class="title"><?php echo $Translate->__('Details');?></h2>
+        <h2 class="title"><?php echo $Parsedown->text($Translate->__('details'));?></h2>
+        <p><?php echo $Parsedown->text($Translate->__('details-venue-meeting'));?></p>
+        <p><?php echo $Parsedown->text($Translate->__('details-venue-banquet'));?></p>
+        <p><?php echo $Parsedown->text($Translate->__('details-public-transport'));?></p>
+        <p><?php echo $Parsedown->text($Translate->__('details-attendance-package'));?></p>
     </div>
 </section>
 
